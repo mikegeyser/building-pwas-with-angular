6d2229eb4a4f8fe9ba7ffc3357d0527adb7afceb
Mike Geyser <mikegeyser@gmail.com>
2018-03-22 12:13:50 +0200

    Added more memes.

--- source/api/index.js
+++ source/api/index.js
@@ -8,24 +8,42 @@ app.use(cors());
 
 const memes = [
     // lotr
-    { id: 1, category: 'lotr', template: "one-does-not-simply.png", top: "One does not simply", bottom: "Upgrade Angular" },
-    { id: 2, category: 'lotr', template: "and-my-axe.jpg", top: "And my", bottom: "directive!" },
+    { id: 1, category: 'lotr', template: "one-does-not-simply.png", top: "One does not simply go", bottom: "offline first" },
+    { id: 2, category: 'lotr', template: "and-my-axe.jpg", top: "And my", bottom: "service worker!" },
     { id: 3, category: 'lotr', template: "you-have-no-power-here.jpg", top: "Internet Explorer", bottom: "You have no power here!" },
-    { id: 4, category: 'lotr', template: "elevenses.png", top: "What do you mean", bottom: "I have to choose a module loader?" },
+    { id: 4, category: 'lotr', template: "elevenses.png", top: "What do you mean", bottom: "cache invalidation?" },
     { id: 5, category: 'lotr', template: "legolas.jpg", top: "I hear the internet", bottom: "losing its mind" },
-    { id: 5, category: 'lotr', template: "mine.jpg", top: "I should have", bottom: "used react!" },
+    { id: 5, category: 'lotr', template: "mine.jpg", top: "I should have", bottom: "gone native!" },
     { id: 6, category: 'lotr', template: "not-this-day.jpg", top: "One day we'll support IE", bottom: "But not this day!" },
     { id: 7, category: 'lotr', template: "oh-no.png", top: "I have to use", bottom: "TypeScript" },
     { id: 8, category: 'lotr', template: "orc.jpg", top: "what do you mean", bottom: "there's no upgrade path?" },
-    { id: 9, category: 'lotr', template: "survived.png", top: "We've done it...", bottom: "We configured WebPack" },
-    { id: 10, category: 'lotr', template: "yes.jpg", top: "But... I like", bottom: "Java!" }
+
+    // sw
+    { id: 31, category: 'sw', template: "speechless-luke.jpg", top: "", bottom: "\"...\"" },
+    { id: 32, category: 'sw', template: "bb-8-thumbs-up.jpg", top: "Works on", bottom: "my machine" },
+    { id: 33, category: 'sw', template: "not-the-droids.jpg", top: "There are no", bottom: "bugs here" },
+    { id: 34, category: 'sw', template: "deathstar-explosion.jpg", top: "Friday afternoon", bottom: "deployments" },
+
+    // corgi
+    { id: 11, category: "corgi", template: "IDrive.jpg", top: "I know", bottom: "the javascripts" },
+    { id: 12, category: "corgi", template: "battle.jpg", top: "tabs vs.", bottom: "spaces" },
+    { id: 13, category: "corgi", template: "corgidown.jpg", top: "Turn it off and", bottom: "on again" },
+    { id: 14, category: "corgi", template: "maxderp.jpg", top: "successful", bottom: "deployment" },
+    { id: 15, category: "corgi", template: "mistake.jpg", top: "Performance", bottom: "is fine" },
+    { id: 16, category: "corgi", template: "treats.jpg", top: "There are no", bottom: "bugs here" },
+
+    // rs
+    { id: 21, category: "rs", template: "ron-swanson.jpg", top: "Would you put your", bottom: "salary off through that" },
+    { id: 22, category: "rs", template: "ron-swanson.jpg", top: "That word doesn't mean", bottom: "what you think it means" },
+    { id: 23, category: "rs", template: "ron-swanson.jpg", top: "as such", bottom: "though" },
+
 ];
 
 const categories = [
     { key: 'lotr', description: 'Lord of the Rings' },
     { key: 'sw', description: 'Star Wars' },
-    { key: 'pr', description: 'Parks and Recreation' },
-    { key: 'corgi', description: 'Corgies' }
+    { key: 'corgi', description: 'Corgies' },
+    { key: 'rs', description: 'Ron Swanson' },
 ];
 
 const base_url = 'public/images';
@@ -45,7 +63,7 @@ const templates = {
         'you-have-no-power-here.jpg'
     ],
     sw: [],
-    pr: [],
+    rs: ['ron-swanson.jpg'],
     corgi: [
         'IDrive.jpg',
         'battle.jpg',
